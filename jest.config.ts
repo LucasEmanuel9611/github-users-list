6591df96bf0ec769bbb6f9ebd14c268409dbe005
Lucas Emanuel <lucas190717@gmail.com>
2023-05-12 11:44:51 -0300

    tests: configure coverage provider

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,17 @@
 module.exports = {
   preset: "ts-jest",
+
   testEnvironment: "node",
+
+  bail: true,
+
+  clearMocks: true,
+
+  coverageProvider: "v8",
+
+  collectCoverage: true,
+
+  coverageDirectory: "coverage",
+
+  coverageReporters: ["text-summary", "lcov"],
 };
